5429402c5ac3af55bf889c92990a5e7d863a0667
黄程宇 <huangchy@shanghaitech.edu.cn>
2020-01-10 01:00:15 +0800

    optimized

--- main.py
+++ main.py
@@ -247,17 +247,17 @@ class PlayField:
             screen.blit(prev_tet_table[tetrominoes.index(self.hold_tet)], (0, 0))
             self.new_piece()
         self.cur_tetromino = Tetromino(action.tet_type)
-        for op, kick in action.moving:
-            start = time()
-            blit_tet(self.cur_tetromino.grid, 'black', self.cur_tetromino.pos)
-            self.cur_tetromino.take_op(op, kick)
-            blit_tet(self.cur_tetromino.grid, self.cur_tetromino.type, self.cur_tetromino.pos)
-            pygame.display.flip()
+        # for op, kick in action.moving:
+        #     start = time()
+        #     blit_tet(self.cur_tetromino.grid, 'black', self.cur_tetromino.pos)
+        #     self.cur_tetromino.take_op(op, kick)
+        #     blit_tet(self.cur_tetromino.grid, self.cur_tetromino.type, self.cur_tetromino.pos)
+        #     pygame.display.flip()
             # sleep(max(0.0, 0.025 - (time() - start)))
 
-        # self.cur_tetromino.grid = action.grid
-        # self.cur_tetromino.rotation = action.rotation
-        # self.cur_tetromino.pos = action.pos
+        self.cur_tetromino.grid = action.grid
+        self.cur_tetromino.rotation = action.rotation
+        self.cur_tetromino.pos = action.pos
 
         self.cur_tetromino.ghost_pos = self.find_ghost_pos()
         if self.place_piece() == 0:
@@ -304,6 +304,21 @@ class PlayField:
         grid,_pos = tet.spawn_tet()
         q.push(tet)
         close_set.add((_pos[0], _pos[1], tet.rotation))
+        field = self.field
+        length = tet.length
+        minIdx = 19
+        for c in range(len(field)):
+            h = 19
+            for i, j in enumerate(field[c]):
+                if j != 0:
+                    h = i - 1
+                    break
+            if h < minIdx:
+                minIdx = h
+        if minIdx - length > tet.pos[1]:
+            tet.pos[1] = minIdx - length
+        for i in range(minIdx - length - _pos[1]):
+            tet.moving.append((0, (0, 0)))
         res = []
         while not q.isEmpty():
             cur = q.pop()
@@ -628,15 +643,16 @@ def first_non_zero(my_list):
 
 
 def game_intro():
-    screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
-    screen.blit(helvetica_big.render('Ready', True, (150, 150, 150)), (32 * 6, 32 * 9))
-    pygame.display.flip()
-    sleep(0.3)
-    screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
-    screen.blit(helvetica_big.render('Go', True, (150, 150, 150)), (32 * 6, 32 * 9))
-    pygame.display.flip()
-    sleep(0.3)
-    screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
+    pass
+    # screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
+    # screen.blit(helvetica_big.render('Ready', True, (150, 150, 150)), (32 * 6, 32 * 9))
+    # pygame.display.flip()
+    # sleep(0.3)
+    # screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
+    # screen.blit(helvetica_big.render('Go', True, (150, 150, 150)), (32 * 6, 32 * 9))
+    # pygame.display.flip()
+    # sleep(0.3)
+    # screen.fill((0, 0, 0), (32 * 6, 32 * 9, 32 * 4, 32 * 3))
 
 
 def blit_stats_constants():
